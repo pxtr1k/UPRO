57a46ae4509f8564ba922301e519f48a445c9fd5
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-02-03 15:05:31 +0100

    Update and rename main.c to 2zad.c

--- ZI 21-22 drugi termin/2zad.c
+++ ZI 21-22 drugi termin/2zad.c
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct vektor_s {
-    int komp_i;
-    int komp_j;
-};
-
 void SumaVektora(struct vektor_s *pok, int n, struct vektor_s *rez){
 
 (*rez).komp_i=0;
@@ -19,8 +11,3 @@ for(int i=0;i<n;i++){
  return;
 }
 
-int main(void)
-{
-    printf("Hello world!\n");
-    return 0;
-}
