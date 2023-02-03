68cb738f8dee661edd6796528acbe252830b5c91
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-02-03 15:52:19 +0100

    Add files via upload

---
+++ ZIR 21-22/main.c
@@ -0,0 +1,16 @@
+void put(int seed, int koraka, struct tocka_s *tocke){
+srand((unsigned int)seed);
+tocke[0].x=0;
+tocke[0].y=0;
+
+
+for(int i=1;i<=koraka;i++){
+tocke[i].x=tocke[i-1].x + rand()%11;
+tocke[i].y=tocke[i-1].y + rand()%11;;
+}
+
+return;
+}
+
+
+
