79029cd9fc073646fbfd7f776fe6308f13572b99
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-02-03 15:51:38 +0100

    Add files via upload

---
+++ ZIR 21-22/main.c
@@ -0,0 +1,33 @@
+int unosKonverzija(char *niz, int *dulj){
+int duljinaniza=0;
+int brojacupitnika=0;
+char zn;
+
+while(zn!='!' && duljinaniza<10){
+scanf("%c", &zn);
+if(zn!='!'){
+
+if(zn>='A' && zn<='Z'){
+zn+=32;
+}else if(zn>='0' && zn<='9'){
+zn='?';
+brojacupitnika++;
+}
+  *(niz+duljinaniza)=zn;
+}else{
+break;
+}
+duljinaniza++;
+}
+
+
+
+
+*(niz+duljinaniza)='\0';
+*dulj=duljinaniza;
+
+
+
+return brojacupitnika;
+
+}
