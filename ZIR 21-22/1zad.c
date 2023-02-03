30c3de198f05ff54defddeea48ae0733c2a545c4
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-02-03 15:54:59 +0100

    Update 1zad.c

--- ZIR 21-22/1zad.c
+++ ZIR 21-22/1zad.c
@@ -1,1 +1,26 @@
+int izgladi(double *p, int m, int n){
 
+    int br = 0;
+    double prethodni[m][n];
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++)
+            prethodni[i][j] = p[n * i + j];
+
+    }
+
+    for (int i = 0; i < m; i++){
+        for (int j = 1; j < n; j++){
+            if (j != (n - 1)){
+                p[n * i + j] = (prethodni[i][j - 1] + p[n * i + j] + p[n * i + j + 1]) / 3;
+
+                if (p[n * i + j] != prethodni[i][j])
+                    br++;
+            }
+
+        }
+    }
+
+    return br;
+
+}
