f0c6bf393767c4b1d8e1ab95ab8792b880290afe
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-02-03 15:06:32 +0100

    Add files via upload

---
+++ ZI 21-22 drugi termin/main.c
@@ -0,0 +1,27 @@
+
+_Bool txt2bin(char *inputFile, char *outputFile){
+struct record r;
+FILE *tokulaz=fopen(inputFile, "r");
+FILE  *tokizlaz=fopen(outputFile, "wb");
+if(tokulaz==NULL){
+    return 0;
+}
+if(tokizlaz==NULL){
+    return 0;
+}
+
+ while(fscanf(tokulaz, "%2d%3d%3d", &r.code, &r.x, &r.y)==3){
+    fseek(tokizlaz, (r.code-1)*sizeof(r), SEEK_SET);
+    fwrite(&r, sizeof(r), 1, tokizlaz);
+ }
+fclose(tokulaz);
+fclose(tokizlaz);
+
+return 1;
+}
+int main(void){
+
+
+
+return 0;
+}
